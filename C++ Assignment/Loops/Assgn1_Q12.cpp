07d21a7679ffac8547e96ed1d31e09eb59606c89
Madhav Sharma <42112240+madhavjk@users.noreply.github.com>
2022-08-31 23:40:13 +0530

    Update Assgn1_Q12.cpp

--- C++ Assignment/Loops/Assgn1_Q12.cpp
+++ C++ Assignment/Loops/Assgn1_Q12.cpp
@@ -1,35 +1,22 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
 int main() {
-	// Your code goes here;
-	
-	int n = 5;
-	
-	for(int i=0; i<n; i++){
-		for(int j=0; j<i+1; j++){
-			
-			
-		cout<<"*"<<' ';
-		}
-		
-		cout<<endl;
-		
-		
-	}
-	
-	for(int i=n; i<0; i--){
-			
-		for(int j =0; j<i-1; j++)
-		{
-			std::cout<<'*'<<' ';
-		}
-			std::cout<<endl;
-		}
-		
-	}
+  // right pasal triangle
+  int size = 5;
 
-	
+  for (int i = 1; i <= size; i++) {
+    for (int j = 0; j < i; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  
+  for (int i = 1; i <= size - 1; i++) {
+    for (int j = 0; j < size - i; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
