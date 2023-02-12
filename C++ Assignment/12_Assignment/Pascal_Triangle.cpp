d87a682c68fac75090eb8c1ba29f81197cbac658
Madhav Sharma <42112240+madhavjk@users.noreply.github.com>
2023-02-12 23:46:53 +0530

    Create Pascal_Triangle.cpp

---
+++ C++ Assignment/12_Assignment/Pascal_Triangle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+
+vector<vector<long long int>> printPascal(int n) 
+{
+          for (int line = 1; line <= n; line++){
+                int C = 1; 
+                for (int i = 1; i <= line; i++){
+                  cout<< C<<" ";
+                  C = C * (line - i) / i;
+              }
+              cout<<"\n";
+}
+              
+              
+  
+}
